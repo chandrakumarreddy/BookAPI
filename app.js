d916d14fd385271f073207e146f793ec8767fb79
chandrakumarreddy <chandrapenugonda655@gmail.com>
2018-09-10 23:02:02 +0530

    gulp configured for the prject

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ app.get("/", (req, res) => {
 	res.send("Home page rendering");
 });
 
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, (req, res) => {
-	console.log("server started at port 3000");
+	console.log(`server started at port ${PORT}`);
 });
---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+const gulp = require("gulp"),
+	nodemon = require("gulp-nodemon");
+
+gulp.task("default", function() {
+	nodemon({
+		script: "app.js",
+		ext: "js",
+		env: { PORT: 8000 },
+		ignore: ["./node_modules/**"]
+	})
+	.on: ("restart", () => console.log("Restarted"));
+});
